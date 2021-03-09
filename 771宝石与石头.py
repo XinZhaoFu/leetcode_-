6ab938a959359ac7a6a89ea33819a4011fcc0c2b
XinZhaoFu <35882457+XinZhaoFu@users.noreply.github.com>
2021-03-09 15:16:12 +0800

    更新

---
+++ 771宝石与石头.py
@@ -0,0 +1,28 @@
+"""
+ 给定字符串J 代表石头中宝石的类型，和字符串 S代表你拥有的石头。 S 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石。
+
+J 中的字母不重复，J 和 S中的所有字符都是字母。字母区分大小写，因此"a"和"A"是不同类型的石头。
+
+示例 1:
+
+输入: J = "aA", S = "aAAbbbb"
+输出: 3
+"""
+
+
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        res = 0
+        stones_dict = {}
+        for ch in stones:
+            stones_dict[ch] = stones_dict.get(ch, 0) + 1
+
+        for ch in jewels:
+            if ch in stones_dict:
+                res += stones_dict[ch]
+        return res
