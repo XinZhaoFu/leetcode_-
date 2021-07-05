6d198900a0c9590308406daa525acd46e61a1260
XinZhaoFu <948244817@qq.com>
2021-07-05 23:35:48 +0800

    更新

---
+++ 3无重复字符的最长子串.py
@@ -0,0 +1,50 @@
+"""
+给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+
+示例 1:
+输入: s = "abcabcbb"
+输出: 3
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+
+示例 2:
+输入: s = "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+
+示例 3:
+输入: s = "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+
+示例 4:
+输入: s = ""
+输出: 0
+
+提示：
+0 <= s.length <= 5 * 104
+s 由英文字母、数字、符号和空格组成
+"""
+
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if len(s) == 0 or len(s) == 1:
+            return len(s)
+        left = 0
+        lookup = set()
+        max_len = 0
+        cur_len = 0
+        for i in range(len(s)):
+            cur_len += 1
+            while s[i] in lookup:
+                lookup.remove(s[left])
+                left += 1
+                cur_len -= 1
+            max_len = max(max_len, cur_len)
+            lookup.add(s[i])
+        return max_len
